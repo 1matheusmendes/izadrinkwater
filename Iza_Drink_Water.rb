64a25e7e48d569be80d04bdf2c0859556da0fee2
Matheus Mendes <matheus_mendess@live.com>
2021-04-10 11:22:49 -0300

    Up

--- Iza_Drink_Water.rb
+++ Iza_Drink_Water.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key = "1FVCSRZPiF8jyxxdjcDYurXuY",
-    config.consumer_secret = "1rcYavR8iSozaF1r1mdIfnv55QQ6IFOWeoZS9400pKB0Iw8ZRg",
-    config.access_token = "1379241175626965001-BCBoGerJzMzdJbJDU4qoy46HXntOol",
-    config.access_token_secret = "Gq9dZ9d9f0RNaG8gcr0arYPU8SBD2pFqS5n0druaznGMO"
+    config.consumer_key = "",
+    config.consumer_secret = "",
+    config.access_token = "",
+    config.access_token_secret = ""
 end
 
 client.update("amor vai beber água");
